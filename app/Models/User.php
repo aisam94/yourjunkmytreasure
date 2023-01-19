c5fa4284e88cf4f2a8b47a6b4436886fd6e8623d
Aisamuddin <aisam0094@gmail.com>
2023-01-20 00:20:28 +0800

    add models

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,8 +17,11 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+    protected $guarded = [];
+
     protected $fillable = [
         'name',
+        'username',
         'email',
         'password',
     ];
@@ -41,4 +44,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
